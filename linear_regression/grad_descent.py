c3d3ce5152fdf601d2ed73203becbb4b788b8ede
Milton <milton.pagan1@upr.edu>
2019-08-29 14:02:04 -0400

    Implement gradient descent algorithm
    Linear regression is now fully implemented for any number of features,
    as required.

--- linear_regression/grad_descent.py
+++ linear_regression/grad_descent.py
@@ -2,26 +2,27 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 class GradDescent(object):
 
-    def __init__(self, learning_rate):
+    def __init__(self, data_set, learning_rate):
         self.learning_rate = learning_rate
 
-
-    def run(self, data_set):
         self.num_samples = len(data_set.index)
 
-        features = data_set.to_numpy()
+        self.features = data_set.to_numpy()
 
-        y_values = features[:, -1]
+        self.y_values = self.features[:, -1]
 
-        features = features[:, :-1]
+        self.features = self.features[:, :-1]
 
         self.parameters = np.zeros(len(data_set.columns) - 1)
 
+    def run(self):
+
         temp_parameters = np.copy(self.parameters)
 
-        test = []
+        costs = []
 
         count = 0
 
@@ -31,46 +32,61 @@ class GradDescent(object):
             for param in self.parameters:
                 summation = 0
 
-                for feature_row, y_val in zip(features, y_values):
+                for feature_row, y_val in zip(self.features, self.y_values):
                     summation += ((np.matmul(self.parameters, feature_row)) - y_val) * feature_row[j]
 
                 temp_parameters[j] = param - self.learning_rate * (1 / self.num_samples) * summation
 
-                print(temp_parameters[j])
-
                 j += 1
-
-
-
-            prev_parameters = np.copy(self.parameters)
 
             self.parameters = np.copy(temp_parameters)
 
-            print(self.parameters)
-
-            test.append(self.cost_function(features, y_values))
+            costs.append(self.cost_function())
+            print(costs[count])
             count += 1
 
-            if self.check_convergence(prev_parameters):
-                plt.plot(np.arange(count), test)
+            if count > 2 and self.check_convergence(*costs):
+                plt.plot(np.arange(count), costs)
                 plt.show()
                 print(count)
                 break
 
+    def check_convergence(self, *costs):
+        return costs[-1] == costs[-2]
 
-
-    def check_convergence(self, prev_parameters):
-
-        for real_parameter, prev_parameter in zip(self.parameters, prev_parameters):
-            if abs(real_parameter - prev_parameter) != 0:
-                return False
-
-        return True
-
-    def cost_function(self, features, y_values):
+    def cost_function(self):
         summation = 0
 
-        for feature_row, y_val in zip(features, y_values):
+        for feature_row, y_val in zip(self.features, self.y_values):
             summation += ((np.matmul(self.parameters, feature_row)) - y_val) ** 2
 
         return (1 / (2 * self.num_samples)) * summation
+
+    def change_dataset(self, data_set):
+
+        self.num_samples = len(data_set.index)
+
+        self.features = data_set.to_numpy()
+
+        self.y_values = self.features[:, -1]
+
+        self.features = self.features[:, :-1]
+
+        self.parameters = np.zeros(len(data_set.columns) - 1)
+
+    def mean_normalize(self):
+
+        for column in self.features.transpose():
+            mean = column.mean()
+            std = column.std()
+
+            for i in range(len(column)):
+                if std != 0:
+                    column[i] = (column[i] - mean) / std
+
+        mean = self.y_values.mean()
+        std = self.y_values.std()
+
+        for i in range(len(self.y_values)):
+            if std != 0:
+                self.y_values[i] = (self.y_values[i] - mean) / std
--- linear_regression/main.py
+++ linear_regression/main.py
@@ -1,18 +1,56 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from grad_descent import *
+import numpy as np
+from linear_regression.grad_descent import *
+from matplotlib import pyplot as plt
+from mpl_toolkits import mplot3d
 
 # Import datasets
 
 dta_set1 = pd.read_csv("/home/milton/Documents/machine-learning-ex1/ex1/ex1data1.txt")
 dta_set2 = pd.read_csv("/home/milton/Documents/machine-learning-ex1/ex1/ex1data2.txt")
 
+# Add X_0 columns
 dta_set1.insert(0, 'X_0', np.ones(len(dta_set1.index)))
-dta_set1.columns = ['X_0','X_1','Y']
+dta_set1.columns = ['X_0', 'X_1', 'Y']
 
 dta_set2.insert(0, 'X_0', np.ones(len(dta_set2.index)))
-dta_set2.columns = ['X_0','X_1', 'X_2','Y']
+dta_set2.columns = ['X_0', 'X_1', 'X_2', 'Y']
 
-grad_descent = GradDescent(0.02)
+grad_descent = GradDescent(dta_set1, 0.02)
 
-grad_descent.run(dta_set1)
+grad_descent.run()
+
+plt.figure(1)
+
+plt.title("One dimensional linear regression")
+
+plt.scatter(dta_set1['X_1'].to_numpy(), dta_set1['Y'].to_numpy())
+
+x = np.linspace(dta_set1['X_1'].min(), dta_set1['X_1'].max(), len(dta_set1.index))
+
+plt.plot(x, [(grad_descent.parameters[1] * i + grad_descent.parameters[0]) for i in x], color='brown')
+
+plt.show()
+
+grad_descent.learning_rate = 0.9
+grad_descent.change_dataset(dta_set2)
+grad_descent.mean_normalize()
+grad_descent.run()
+
+fig = plt.figure(2)
+ax = plt.axes(projection='3d')
+
+plt.title("Two dimensional linear regression")
+
+ax.scatter3D(dta_set2['X_1'].to_numpy(), dta_set2['X_2'].to_numpy(), dta_set2['Y'].to_numpy(), c=dta_set2['Y'].to_numpy())
+
+x = np.linspace(dta_set2['X_2'].min(), dta_set2['X_2'].max(), len(dta_set2.index))
+y = np.linspace(dta_set2['X_1'].min(), dta_set2['X_1'].max(), len(dta_set2.index))
+
+X, Y = np.meshgrid(x, y)
+
+Z = np.array([(grad_descent.parameters[2] * x + grad_descent.parameters[1] * y + grad_descent.parameters[0]) for x, y in zip(X, Y)])
+
+ax.plot_wireframe(X, Y, Z, color='green')
+
+plt.show()
